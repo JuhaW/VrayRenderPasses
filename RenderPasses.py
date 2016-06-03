df2ca27e84b03e756d8f749548949cac28c08152
TynkaTopi <topi__@hotmail.com>
2016-06-03 15:49:09 +0300

    Init

---
+++ RenderPasses.py
@@ -0,0 +1,199 @@
+import bpy
+import operator
+from vb30.plugins import PLUGINS_ID
+
+
+def node_add(nodetree, node, newnodetype):
+	newnode = nodetree.nodes.new(type=newnodetype)
+	newnode.location.x = node.location.x - 200
+	newnode.location.y = node.location.y + 70
+
+	# get linktree
+	links = nodetree.links
+	# link new node to existing node
+	link = links.new(newnode.outputs[0], node.inputs[0])
+
+
+class RPSettings:
+
+	def getAttrDesc(plugin, attrName):
+		for attrDesc in plugin.PluginParams:
+			if attrDesc['attr'] == attrName:
+				return attrDesc
+
+
+
+	RPassCustom = [0,
+				   1, 21, 18,
+				   2, 15, 14,
+				   3, 17, 16,
+				   6, 4, 7,
+				   11, 23, 24,
+				   8, 10, 9,
+				   5, 12, 22,
+				   20, 19, 25,
+				   27, 26, 13,
+				   28, 29, 30, 31,
+				   32, 33, 34,
+				   35, 36, 37, 38, 39, 40, 41, 42]
+
+	RenderPasses = []
+	# RenderPasses = [i['items'][:2] for i in PLUGINS_ID['RenderChannelColor'].PluginParams if i['attr'] == 'alias']
+
+	# RenderChannelObjectSelect ?
+	RPassOther = ['RenderChannelBumpNormals', 'RenderChannelColorModo', 'RenderChannelCoverage',
+				  'RenderChannelDRBucket', 'RenderChannelExtraTex', 'RenderChannelGlossiness',
+				  'RenderChannelMultiMatte', 'RenderChannelNodeID', 'RenderChannelNormals',
+				  'RenderChannelObjectSelect',
+				  'RenderChannelRenderID', 'RenderChannelVelocity', 'RenderChannelZDepth',
+				  'RenderChannelDenoiser']
+
+	bval = [False, True, True]
+
+	bicons ={0:['RADIOBUT_OFF','RADIOBUT_ON', 'RADIOBUT_OFF'],
+			'RenderChannelExtraTex':['ZOOMIN','RADIOBUT_ON', 'ANTIALIASED']}
+	btext = {0:[''],'RenderChannelExtraTex':['ExtraTex','ExtraTex', 'ExtraTex/AO']}
+
+	def passes():
+
+		# clear render passes booleans
+		bpy.context.scene.bool = ""
+
+		attrDesc = RPSettings.getAttrDesc(PLUGINS_ID['RenderChannelColor'], 'alias')
+		if attrDesc:
+			RPSettings.RenderPasses = [i[:2] for i in attrDesc['items']]
+			for i in RPSettings.RenderPasses:
+				bpy.context.scene.bool += "0"
+
+			#print(RPSettings.RenderPasses)
+			# sort by 2.item
+			RPSettings.RenderPasses.sort(key=operator.itemgetter(1))
+
+			for i in RPSettings.RPassOther:
+				bpy.context.scene.bool += "0"
+
+
+class ClearPasses(bpy.types.Operator):
+	bl_idname = 'clear.passes'
+	bl_label = "Clear passes"
+
+	#print ("init ClearPasses")
+
+	def node_create_color_channel(self, node_out, nodetree, RenderPasses):
+
+		color_channel = []
+		sizex = [140, 87]
+		sizey = [176, 45]
+
+		# create render pass nodes
+
+		for i, passes in enumerate(RenderPasses):
+			node = nodetree.nodes.new(type='VRayNodeRenderChannelColor')
+
+			# render pass type
+			node.RenderChannelColor.alias = RenderPasses[i][0]
+
+			node.location.x = (i % 4) * sizex[1]
+			node.location.y = 700 - (((i % 4 * sizey[1]) / 2.5) + ((int(i / 4) * sizey[1]) * 2))
+
+			node.hide = True
+			node.select = False
+			# node name to RenderPasses names
+			node.name = RPSettings.RenderPasses[i][1]
+
+			# add input socket
+			node_out.inputs.new(name='Channel', type='VRaySocketRenderChannel')
+			# set renderpass off
+			node_out.inputs[i].use = False
+			# add socket name to RenderPasses names
+			node_out.inputs[i].name = RPSettings.RenderPasses[i][1]
+
+			# get linktree
+			links = nodetree.links
+			# link color nodes to Render channel node
+			link = links.new(node.outputs[0], node_out.inputs[i])
+
+			color_channel.append(node)
+
+		return
+
+	def node_create_renderid(self, node_out, nodetree, RenderPasses):
+
+		sizex = [140, 87]
+		sizey = [176, 45]
+
+		for i, nlist in enumerate(RPSettings.RPassOther):
+			name = [nlist][0][13:]
+			#print("name:", name)
+			nodetype = 'VRayNode' + nlist
+			node = nodetree.nodes.new(type=nodetype)
+			node.name = name
+
+			node.location.x = (i % 4) * sizex[1]
+			node.location.y = -270 - (((i % 4 * sizey[1]) / 2.5) + ((int(i / 4) * sizey[1]) * 2))
+
+			node.hide = True
+			node.select = False
+
+			# add input socket
+			input = node_out.inputs.new(name='Channel', type='VRaySocketRenderChannel')
+			# set renderpass off
+			input.use = False
+			# add socket name to RenderPasses names
+			input.name = name
+
+			# get linktree
+			links = nodetree.links
+			# link color nodes to Render channel node
+			link = links.new(node.outputs[0], input)
+
+			# if it is ExtraTex
+			if node.vray_plugin == 'RenderChannelExtraTex':
+				# print('RenderChannelExtraTex')
+				if not node.inputs[0].links:
+					# create textdirt node
+					node_add(nodetree, node, 'VRayNodeTexDirt')
+
+
+		return
+
+	def execute(self, context):
+
+		#print("RenderPasses.ClearPasses.execute")
+		for area in bpy.context.screen.areas:
+			if area.type == "NODE_EDITOR":
+				override = {'screen': bpy.context.screen, 'area': area}
+
+		RPSettings.passes()
+		ClearPasses(bpy.context)
+		scn = bpy.context.scene
+		# clear render passes UI booleans
+		# RenderPasses = RenderChannelColor.ColorChannelNamesMenu
+		# scn.RPass = [False for x in range(len(RPSettings.RenderPasses))]
+		# scn.RPassOther = [False for x in range(len(RPSettings.RPassOther))]
+
+		ng = bpy.data.node_groups
+		LName = 'RenderPasses'
+		RLayer = ng.new(LName, 'VRayNodeTreeScene') if LName not in ng else ng[LName]
+		bpy.context.scene.vray.ntree = RLayer
+
+		# get nodetree
+		nodetree = bpy.context.scene.vray.ntree
+		nodes = nodetree.nodes
+		nodes.clear()
+		# create Render Channel node
+		node_out = nodes.new('VRayNodeRenderChannels')
+		# clear all input sockets
+		node_out.inputs.clear()
+
+		node_out.location.x = 600
+		node_out.location.y = 800
+		node_out.select = False
+
+		self.node_create_color_channel(node_out, nodetree, RPSettings.RenderPasses)
+		self.node_create_renderid(node_out, nodetree, RPSettings.RenderPasses)
+
+		# clear Light passes
+		scn.prop_group.coll.clear()
+
+		return {'FINISHED'}
---
+++ __init__.py
@@ -0,0 +1,234 @@
+bl_info = {
+	"name": "Vray Render Passes",
+	"author": "JuhaW",
+	"version": (0, 1, 0),
+	"blender": (2, 77, 0),
+	"location": "Tools",
+	"description": "Easier render/light passes",
+	"warning": "beta",
+	"wiki_url": "",
+	"category": "",
+}
+# --------------------------------------------------------------------------
+import importlib
+
+if "LP" in locals():
+	importlib.reload(RP)
+	importlib.reload(LP)
+	print("Reload:", LP)
+
+import bpy  # , vb30
+import operator
+from vb30.plugins import PLUGINS_ID
+from bpy.props import IntProperty, IntVectorProperty, StringProperty, BoolProperty, PointerProperty, BoolVectorProperty
+from bpy.types import PropertyGroup
+from . import RenderPasses as RP
+from . LightPass import LightPass as LP
+# --------------------------------------------------------------------------
+
+
+def update_NodeDefaults(self, context):
+	# print ('NodeDefaults update')
+	# UpdateNodes.execute(self,context)
+	color_mapping = ['Diffuse',
+	                 'Specular',
+	                 'Reflection Filter',
+	                 'Refraction Filter',
+	                 'Background']
+
+	nodes = bpy.data.node_groups['RenderPasses'].nodes
+	# set node colormapping
+	colorbool = False if bpy.context.scene.NodeDefaults else True
+	for i in nodes:
+		if i.name in color_mapping:
+			# print("passes colormapping:", i.name)
+			i.RenderChannelColor.color_mapping = colorbool
+
+
+def update_prop_group(self, context):
+	# print("prop update:")
+	pass
+
+
+class Test(bpy.types.Operator):
+	bl_idname = 'test.operator'
+	bl_label = "Test operator"
+
+	boolvar = IntProperty()
+
+
+	def execute(self, context):
+		# print(self.boolvar)
+
+		# select node
+		nodetree = bpy.data.node_groups['RenderPasses']
+		nodes = nodetree.nodes
+		nodeout = nodes.get('Render Channles Container')
+
+		node = nodeout.inputs[self.boolvar].links[0].from_node
+		for i in nodes:
+			i.select = False
+			if i != nodeout:
+				i.hide = True
+
+		node.select = True
+		node.hide = False
+		nodes.active = node
+		if node.inputs and node.inputs[0].links:
+			node.inputs[0].links[0].from_node.hide = False
+
+		# reverse bool value
+		b = context.scene.bool
+		context.scene.bool = b[:self.boolvar] + str(int(b[self.boolvar]) ^ 1) + b[self.boolvar + 1:]
+
+		UpdateNodes.execute(self, context)
+
+		return {'FINISHED'}
+
+
+class SettingsOperator(bpy.types.Operator):
+	bl_idname = 'settings.operator'
+	bl_label = "Test operator"
+
+
+	def execute(self, context):
+		Var.settings = not Var.settings
+		#print (self.boolvar)
+
+		return {'FINISHED'}
+
+
+class Var():
+
+	settings = BoolProperty(default = True)
+	settings = True
+
+
+
+class RenderPassPanel(bpy.types.Panel):
+	"""Creates a Panel in the Tools panel"""
+	bl_label = "Vray Render Passes"
+	bl_idname = "renderpass.panel"
+	bl_space_type = 'PROPERTIES'
+	bl_region_type = 'WINDOW'
+	bl_context = "render_layer"
+
+
+	def draw(self, context):
+
+		layout = self.layout
+		sce = context.scene
+		row = layout.row()
+
+		row.operator('settings.operator', text="Settings", icon='VRAY_LOGO' if Var.settings else 'VRAY_LOGO_MONO' , emboss=True)
+		#VRAY_LOGO, VRAY_LOGO_MONO,SCRIPTPLUGINS
+		if Var.settings:
+			box = layout.box()
+			row = box.row()
+
+			row.operator('clear.passes')
+			row.prop(sce, "RPassSwitch", text="Passes on")
+			# row = layout.row()
+			row.prop(sce, "NodeDefaults", text="Default")
+			row.prop(sce, "RenderPassColumns", text="2/3 Columns")
+
+		col = layout.column_flow(columns=3 if sce.RenderPassColumns else 2, align=True)
+		col.enabled = sce.RPassSwitch
+
+		# RenderPasses = bpy.data.objects[V.name].constraints
+		RenderPasses = RP.RPSettings.RenderPasses
+		# RenderPasses = RenderChannelColor.ColorChannelNamesMenu
+		# col.alert = False
+
+		if sce.bool != None and RenderPasses:
+			# o = bpy.data.objects[V.name]
+
+			icon_on = 'RADIOBUT_ON'
+			icon_off = 'RADIOBUT_OFF'
+
+			for i in range(len(sce.bool)):
+
+				bo = int(context.scene.bool[i])
+				b = RP.RPSettings.bval[bo]
+
+				if i < len(RenderPasses):
+					bool = col.operator('test.operator', text=str(RenderPasses[i][1]),emboss=b,	icon=icon_on if b else icon_off)
+					bool.boolvar = i
+				else:
+					diff = i - len(RenderPasses)
+					bool = col.operator('test.operator', text=RP.RPSettings.RPassOther[diff][13:], emboss=b,icon=icon_on if b else icon_off)
+					bool.boolvar = i
+
+		# LIGHT PASS
+		layout = self.layout
+		sce = context.scene
+		row = layout.row()
+		row.enabled = sce.RPassSwitch
+		row.operator("lightpass.add", icon='OUTLINER_OB_LAMP')
+		# row.operator("lightpass.update")
+		# layout.template_list("SCENE_UL_list", "", sce.prop_group, "coll", sce.prop_group, "index","", sce.prop_group.index)
+		layout.template_list("SCENE_UL_list", "", sce.prop_group, "coll", sce.prop_group, "index", "",
+			sce.prop_group.index)
+
+
+class RenderPassGroup(bpy.types.PropertyGroup):
+	juha = bpy.props.IntProperty()
+
+
+def renderpass_onoff(self, context):
+	scn = bpy.context.scene
+	nodeout = scn.vray.ntree.nodes.get('Render Channles Container')
+	RenderPasses = RP.RPSettings.RenderPasses
+
+	pass_amount = len(context.scene.bool)
+	# normal render passes
+	for i in range(pass_amount):
+		if scn.RPassSwitch:
+			nodeout.inputs[i].use = int(context.scene.bool[i])
+		else:
+			nodeout.inputs[i].use = False
+
+	# Light passes
+	for i, passes in enumerate(scn.prop_group.coll):
+		if scn.RPassSwitch:
+			nodeout.inputs[i + pass_amount].use = True
+		else:
+			nodeout.inputs[i + pass_amount].use = False
+		# print("light passes:", len(scn.prop_group.coll))
+
+
+class UpdateNodes(bpy.types.Operator):
+	bl_idname = 'update.nodes'
+	bl_label = "Update nodes"
+
+	def execute(self, context):
+		LP.renderpass_bool(self, context)
+		LP.LightPassUpdate.execute(self, context)
+
+		return {'FINISHED'}
+
+
+def register():
+	bpy.utils.register_module(__name__)
+
+	bpy.types.Scene.RPassSwitch = BoolProperty(default=True, update=renderpass_onoff)
+	bpy.types.Scene.NodeDefaults = BoolProperty(default=False, update=update_NodeDefaults,description = "Default On: All render pass nodes are with default values \nDefault Off: Set color mapping On for Diffuse,Specular,Reflection filter,Refraction filter and Background passes, for combining render passes,")
+
+	# LP
+	bpy.types.Scene.prop_group = PointerProperty(type=LP.LPGroup, update=update_prop_group)
+
+	bpy.types.Scene.bool = StringProperty()
+	bpy.types.Scene.RenderPassColumns = BoolProperty(default=True)
+
+
+def unregister():
+	bpy.utils.unregister_module(__name__)
+
+	del bpy.types.Scene.RPassSwitch
+	del bpy.types.Scene.NodeDefaults
+	del bpy.types.Scene.bool
+	# LP
+	bpy.types.Scene.prop_group
+
+	del bpy.types.Scene.RenderPassColumns
+	print("unregister Vray Passes addon")
